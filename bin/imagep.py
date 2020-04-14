f1ddc79462ef396d007a1812dddc709aa271c8ee
mstadler <MStadler@Michaels-MacBook-Pro.local>
2020-04-13 18:28:46 -0700

    test

--- bin/imagep.py
+++ bin/imagep.py
@@ -12,40 +12,50 @@ import numpy as np
 import os
 from os import listdir
 from os.path import isfile, join
+import re
 from skimage import filters, io
 from ipywidgets import interact, IntSlider, Dropdown, IntRangeSlider, fixed
 import matplotlib.pyplot as plt
 
 ############################################################################
+# Functions for loading TIFF stacks
+############################################################################
 
-def read_tiff_stack(stack_folder):
+# Main function for loading TIFF stacks
+def _read_tiff_stack(tif_folder, tif_files, **kwargs):
     """Read a folder of 2D or 3D TIFF files into a numpy ndarray.
     
     Args:
-        stack_folder: string
-            Directory containing multiple TIFF files. Must be in asciibetical
-            order.
+        tif_folder: string
+            Directory containing multiple TIFF files.
+        tif_files: list
+            List of files in the folder to load. Must be sorted in order
+            desired.
+        span: tuple of ints
+            Optional key-word argument specifying first and last file to 
+            load, both inclusive. Example: span=(0, 5) loads the first 6 
+            images, numbers 0 through 5.
     
     Returns:
         stack: ndarray
             n-dimensional image stack with the new dimension (file number) 
-            in the 0 position(file_num, x, y, z) for 3D stacks, (filenum, x,
+            in the 0 position(file_num, z, x, y) for 3D stacks, (filenum, x,
             y) for 2D stacks
             
     Raises:
-        ValueError: if dimensions of TIFF file don't match those of the first
-        file
+        ValueError: 
+            If dimensions of TIFF file don't match those of the first
+            file
     """
-    
-    # Compile files that are files and have .tif extension (case-insensitive).
-    tif_files = [f for f in listdir(stack_folder) if (isfile(join(stack_folder, f)) 
-        and (os.path.splitext(f)[1][0:4].upper() == '.TIF'))]
-    # Sort the files: asciibetical sorting produces files ascending in time 
-    # (sorting is *in place*)
-    tif_files.sort()
-    
+    if 'span' in kwargs:
+        first, last = (kwargs['span'])
+        if (first < last) and (last < len(tif_files)):
+            tif_files = tif_files[first:(last + 1)]
+        else:
+            raise ValueError('Span exceeds the dimensions of the stack')
+
     # Create stack with dimensions from first file.
-    img = io.imread(join(stack_folder, tif_files[0]))
+    img = io.imread(join(tif_folder, tif_files[0]))
     dims = img.shape
     num_files = len(tif_files)
     stack = np.ndarray(((num_files,) + dims), dtype=img.dtype)
@@ -55,7 +65,7 @@ def read_tiff_stack(stack_folder):
     # Add the rest of the files to the stack.
     for tif_file in tif_files[1:]:
         # Add image data to ndarray
-        img = io.imread(join(stack_folder, tif_file))
+        img = io.imread(join(tif_folder, tif_file))
         # Check dimensions
         if not stack[0].shape == img.shape:
             raise ValueError(f'Dimensions do not match previous files: {tif_file}')
@@ -64,6 +74,86 @@ def read_tiff_stack(stack_folder):
         
     return stack
 
+# Wrapper function for loading all TIFF files in a folder
+def read_tiff_folder(tif_folder):
+    """Read all TIFF files in a folder into an ndarray.
+    
+        Args:
+            tif_folder: string
+                Directory containing multiple TIFF files. Must be sortable
+                asciibetically.
+            span: tuple of ints
+                Optional key-word argument specifying first and last file to 
+                load, both inclusive. Example: span=(0, 5) loads the first 6 
+                images, numbers 0 through 5.
+        
+        Returns:
+            stack: ndarray
+                n-dimensional image stack with the new dimension (file number) 
+                in the 0 position(file_num, z, x, y) for 3D stacks, (filenum, 
+                x, y) for 2D stacks
+                
+        Raises:
+            ValueError: 
+                If dimensions of TIFF file don't match those of the first
+                file
+    """
+    
+    # Compile files that are files and have .tif extension (case-insensitive).
+    tif_files = [f for f in listdir(tif_folder) if (isfile(join(tif_folder, f)) 
+        and (os.path.splitext(f)[1][0:4].upper() == '.TIF'))]
+    # Sort the files: asciibetical sorting produces files ascending in time 
+    # (sorting is *in place*)
+    tif_files.sort()
+    return _read_tiff_stack(tif_folder, tif_files)
+
+# Wrapper function for loading TIFF files in a lattice-style folder, with
+# CamA and CamB channels.
+def read_tiff_lattice(tif_folder, **kwargs):
+    """Read all TIFF files in a lattice output folder into an ndarray.
+    
+        Args:
+            tif_folder: string
+                Directory containing multiple TIFF files with 'CamA' and 'CamB' 
+                Must be equal numbers of CamA and CamB and files must be 
+                sortable asciibetically.
+            span: tuple of ints
+                Optional key-word argument specifying first and last file to 
+                load, both inclusive. Example: span=(0, 5) loads the first 6 
+                images, numbers 0 through 5.
+        
+        Returns:
+            stack: ndarray
+                n-dimensional image stack with the new dimension (channel) 
+                in the 0 position, e.g. (channel, t, z, x, y) for 3D stacks. 
+                
+        Raises:
+            ValueError: 
+                If dimensions of TIFF file don't match those of the first file
+            ValueError: 
+                If there are non-identical numbers of CamA and CamB files
+    """
+    
+    # Compile files that are files and have .tif extension (case-insensitive).
+    tif_files = [f for f in listdir(tif_folder) if (isfile(join(tif_folder, f)) 
+        and (os.path.splitext(f)[1][0:4].upper() == '.TIF'))]
+    # Sort files into two lists based on containing 'CamA' and 'CamB' in filename.
+    regex_camA = re.compile('CamA')
+    regex_camB = re.compile('CamB')
+    camA_files = [*filter(regex_camA.search, tif_files)] # This syntax unpacks filter into a list.
+    camB_files = [*filter(regex_camB.search, tif_files)]
+
+    # Sort the files: asciibetical sorting produces files ascending in time 
+    # (sorting is *in place*)
+    camA_files.sort()
+    camB_files.sort()
+    # Read both sets of files, combine if they are of same dimensions.
+    camA_stack = _read_tiff_stack(tif_folder, camA_files, **kwargs)
+    camB_stack = _read_tiff_stack(tif_folder, camB_files, **kwargs)
+    if camA_stack.shape == camB_stack.shape:
+        return np.stack((camA_stack, camB_stack), axis=0)
+    else:
+        raise ValueError('Unequal number of CamA and CamB files.')
 ############################################################################
 
 def viewer(stack, order='tzxy'):
@@ -79,7 +169,11 @@ def viewer(stack, order='tzxy'):
     Returns: none
         
     Raises:
-        ValueError: if final two dimensions are not xy
+        ValueError: 
+            If final two dimensions are not xy
+        ValueError:
+            If the number of dimensions in the order string does not match
+            the dimensions of the stack
     """
     # Update the displayed image with inputs from widgets.
     def _update_view(order, **kwargs):
@@ -99,18 +193,20 @@ def viewer(stack, order='tzxy'):
         # Slice stack, leaving last two dimensions for image.
         # Note: the (...,) in the following is not required, but I think 
         # it is clarifying.
-        plt.imshow(stack[tuple(indexes) + (...,)], cmap=colmap, vmin=min_, vmax=max_);    
+        plt.imshow(stack[tuple(indexes) + (...,)], cmap=colmap, vmin=min_, 
+            vmax=max_);    
     
     # Make a new slider object for dimension selection and return it
     def _make_slider(n):
-        widget = IntSlider(min=0, max=(stack.shape[n] - 1), step=1, continuous_update=False,)
+        widget = IntSlider(min=0, max=(stack.shape[n] - 1), step=1, 
+            continuous_update=False,)
         return(widget)
     
     # Make a dropdown widget for selecting the colormap.
     def _make_cmap_dropdown():
         dropdown = Dropdown(
-            options={'viridis', 'plasma', 'magma', 'inferno','cividis','Greens', 
-                     'Reds', 'gray', 'gray_r'},
+            options={'viridis', 'plasma', 'magma', 'inferno','cividis',
+                'Greens', 'Reds', 'gray', 'gray_r'},
             value='viridis',
             description='Color',
         )
@@ -137,6 +233,8 @@ def viewer(stack, order='tzxy'):
     def main(order):
         if not (order[-2:] == 'xy'):
             raise ValueError("Dimension order must end in 'xy'.")
+        if not (len(order) == len(stack.shape)):
+            raise ValueError("Order string doesn't match dimensions of stack")
         # Split order string (minus trailing 'xy') into list.
         order_arr = [char for char in order[:-2]]
         
