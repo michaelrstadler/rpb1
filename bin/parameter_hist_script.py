e3db53a32857751799bb3a6c1c48cc183f01c7c5
Michael Stadler <michaelrstadler@gmail.com>
2021-10-01 10:46:06 -0700

    Added more memory efficient make_scalespace_2dhist function

--- bin/parameter_hist_script.py
+++ bin/parameter_hist_script.py
@@ -4,14 +4,27 @@ import numpy as np
 import flymovie
 from time import time, process_time
 
+from dask.distributed import Client
+if __name__ == "__main__":
+    client = Client(n_workers=4)
+
 outfile = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/results/histparams_test2.pkl'
+"""
 bg_mean_range=[9000, 9500, 10500, 11000, 11500]
-bg_var_range=[600, 800, 1000,1200, 1400]
-blob_intensity_mean_range=np.arange(20000, 30000, 1000)
-blob_intensity_var_range=np.arange(1000,10_000, 1000)
-blob_radius_mean_range=np.arange(1, 5)
+bg_var_range=[600]
+blob_intensity_mean_range=[20000]
+blob_intensity_var_range=[5000]
+blob_radius_mean_range=[3]
 blob_radius_var_range=[0.5]
-blob_number_range=np.arange(0, 500, 25)
+blob_number_range=[250]
+"""
+bg_mean_range=[9000, 9500, 10500, 11000, 11500]
+bg_var_range=[600]
+blob_intensity_mean_range=[30000]
+blob_intensity_var_range=[10000]
+blob_radius_mean_range=[5]
+blob_radius_var_range=[0.5]
+blob_number_range=[500]
 
 t_start = time()
 data_delayed = fm.make_parameter_hist_data(bg_mean_range, bg_var_range, blob_intensity_mean_range, 
