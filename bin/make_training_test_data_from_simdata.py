fb8d91462e28e1b3d44fbd648acb2687944d1efa
Michael Stadler <michaelrstadler@gmail.com>
2021-11-18 22:42:12 -0800

    Changed make_simdifference_data.py to make_training_test_data_from_simdata.py, now takes an output from simulation (histogram and params) and prepares training and test data for learning parameters and image differences.

--- bin/make_training_test_data_from_simdata.py
+++ bin/make_training_test_data_from_simdata.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""make_simdifference_data.py.
+"""make_training_test_data_from_simdata.py.
 
 
 """
@@ -16,28 +16,29 @@ from time import time, process_time
 
 def parse_options():
     parser = OptionParser()
-    parser.add_option("-s", "--sim_files", dest="sim_files",
+    parser.add_option("-s", "--sim_file", dest="sim_file",
                       help="Pickled simulation datasets, comma-separated.", 
                       metavar="SIMS")
-    parser.add_option("-o", "--outfile", dest="outfile",
-                      help="File to save pickled output.", 
-                      metavar="OUTFILE")
+    parser.add_option("-o", "--outfilestem", dest="outfilestem",
+                      help="File stems for saving pickled output.", 
+                      metavar="OUTFILESTEM")
     parser.add_option("-n", "--num_sample", dest="num_sample",
                       help="Number of sims to sample.", 
                       metavar="NUMSAMPLE")
     parser.add_option("-m", "--num_diff", dest="num_diff",
                       help="Number of differences to compute for each sampled sim.", 
                       metavar="NUMDIFF")
+    parser.add_option("-l", "--leaveout_fraction", dest="leaveout_fraction",
+                      help="Fraction of entries to leave out.", 
+                      metavar="LEAVEOUT")
     
     (options, args) = parser.parse_args()
     return options
 
-def prepare_hist_param_data(data, num_hist_bins=400, param_pos=0, data_pos=1):
-    """Process data output from blob simulator (list of tuples, 
-    0 is params, 1 is 2d histogram) for feeding to ML function. 
+def prepare_hist_param_data(data, param_pos, data_pos):
+    """Process data output from blob simulator for feeding to ML function. 
     
     For histograms: 
-        - Bin (if desired)
         - Normalize each 1d histogram by dividing by sum (-> probabilities)
         - Flatten
         - Take log (after adding a bit to avoid 0)
@@ -45,17 +46,12 @@ def prepare_hist_param_data(data, num_hist_bins=400, param_pos=0, data_pos=1):
     For parameters:
         - Combine into single 2d numpy array (rows are entries)
         - Normalize each column (mean 0 sd 1)
-    
-         """
+    """
     def check2d(a):
         """Add dummy first dimension if array is 1d."""
         if len(a.shape) < 2:
             a = np.expand_dims(a, axis=0)
         return a
-
-    def rebin(a, shape):
-        sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
-        return a.reshape(sh).mean(-1).mean(1)
 
     def norm_columns(x):
         """Normalize columns to mean=0 SD=1."""
@@ -68,18 +64,19 @@ def prepare_hist_param_data(data, num_hist_bins=400, param_pos=0, data_pos=1):
                 x1[:,i] = np.zeros(len(x1[:,i]))
         return x1
 
-    num_sigmas = check2d(data[0][data_pos]).shape[0]
+    # Get some relevant values.
+    total_hist_bins = check2d(data[0][data_pos]).size
     num_params = len(data[0][param_pos])
     num_entries = len(data)
-    histograms_processed = np.zeros((num_entries, num_sigmas * num_hist_bins))
+    # Initialize data containers.
+    histograms_processed = np.zeros((num_entries, total_hist_bins))
     params = np.zeros((num_entries, num_params))
 
     for i in range(num_entries):
         params[i] = data[i][param_pos]
+        # Make a copy and ensure histogram is 2D.
         hist2d = data[i][data_pos].copy()
         hist2d = check2d(hist2d)
-        # Bin histograms.
-        hist2d = rebin(hist2d, [hist2d.shape[0], num_hist_bins])
         # Normalize by dividing by row sum.
         hist2d = np.apply_along_axis(lambda x: x / np.sum(x), axis=1, arr=hist2d)
         # Flatten.
@@ -89,16 +86,19 @@ def prepare_hist_param_data(data, num_hist_bins=400, param_pos=0, data_pos=1):
         hist_flatlog = np.log(hist_flattened + zero_pad)
         histograms_processed[i] = hist_flatlog
 
+    # Normalize parameters once they are all collected in array.
     params = norm_columns(params)    
     return histograms_processed, params
 
-def create_training_test_data(data, num_hist_bins=100, n_leaveout=100, param_pos=0, data_pos=1, permute=True):
+def create_training_test_data(data, n_leaveout, param_pos, data_pos, permute):
     """Randomly select entries for training and test data."""
-    histograms, params = prepare_hist_param_data(data, num_hist_bins, param_pos, data_pos)
+    histograms, params = prepare_hist_param_data(data, param_pos, data_pos)
+    # Generate a list of indexes (either randomly permuted or in order).
     if permute:
         p = np.random.permutation(histograms.shape[0])
     else:
         p = np.arange(0, histograms.shape[0])
+
 
     if n_leaveout > 0:       
         x_train = histograms[p[:-n_leaveout]]
@@ -107,9 +107,9 @@ def create_training_test_data(data, num_hist_bins=100, n_leaveout=100, param_pos
         y_test = params[p[-n_leaveout:]]
         
         return x_train, y_train, x_test, y_test
+
     else:
-        x_train, y_train = histograms, params
-        return x_train, y_train
+        raise ValueError('Cannot have 0 leaveouts.')
 
 def merge_data(x_train1, x_train2, y_train1, y_train2):
     """Horizontally merge training data based on common y entries."""
@@ -132,40 +132,58 @@ def make_similarity_data(x_in, y_in, num_sample, num_diff):
     """Use random sampling to generate training data using differences
     between entries."""
     total = num_sample * num_diff
+    # Initialize containers for difference data.
     x = np.zeros((total, x_in.shape[1]))
     y = np.zeros((total, 1))
+    # Sample indexes for the first time to get the entries for comparison.
     indexes = np.arange(0, x_in.shape[0])
     sampled_indexes1 = np.random.choice(indexes, size=num_sample, replace=False)
     for i in range(len(sampled_indexes1)):
-        start = i * num_diff
-        end = start + num_diff
         index1 = sampled_indexes1[i]
+        # Sample indexes for entries to compare this entry to, excluding self.
         indexes_sansself = np.concatenate((indexes[:index1], indexes[(index1 + 1):]))
         sampled_indexes2 = np.random.choice(indexes_sansself, size=num_diff, replace=False)
+        # Generate arrays with data matching second set of sampled indexes.
         x_sub = x_in[sampled_indexes2]
         y_sub = y_in[sampled_indexes2]
+        # Get differences between reference entry and sampled comparison entries.
         x_diffs = x_sub - x_in[index1]
         y_diffs = np.sum((y_sub - y_in[index1]) ** 2, axis=1) # sum of squared differences
+        # Assign difference scores to empty positions in container.
+        start = i * num_diff
+        end = start + num_diff
         x[start:end] = x_diffs
         y[start:end] = np.expand_dims(y_diffs, axis=1)
     return x, y
-    
-t_start = time()
-options = parse_options()
 
-sim_files = options.sim_files.split(',')
+if __name__ == '__main__':
+        
+    t_start = time()
+    options = parse_options()
 
-data = fm.load_pickle(sim_files[0])
-x_train, y_train = create_training_test_data(data, data[0][1].shape[1], param_pos=0, data_pos=1, permute=False, n_leaveout=0)
+    sim_file = options.sim_file
+    num_sample = int(options.num_sample)
+    num_diff = int(options.num_diff)
+    num_sample_test = int(np.sqrt(num_sample))
+    num_diff_test = int(np.sqrt(num_diff))
+    leaveout_fraction = float(options.leaveout_fraction)
 
-for sim_file in sim_files[1:]:
     data = fm.load_pickle(sim_file)
-    x_train_new, y_train_new = create_training_test_data(data, data[0][1].shape[1], param_pos=0, data_pos=1, permute=False, n_leaveout=0)
-    x_train, y_train = merge_data(x_train, x_train_new, y_train, y_train_new)
+    num_leaveout = int(leaveout_fraction * len(data))
+    x_train, y_train, x_test, y_test = create_training_test_data(data, param_pos=0, data_pos=1, permute=False, n_leaveout=num_leaveout)
 
-num_sample = int(options.num_sample)
-num_diff = int(options.num_diff)
+    num_sample = int(options.num_sample)
+    num_diff = int(options.num_diff)
 
-x, y = make_similarity_data(x_train, y_train, num_sample, num_diff)
+    x_train_diff, y_train_diff = make_similarity_data(x_train, y_train, num_sample, num_diff)
+    x_test_diff, y_test_diff = make_similarity_data(x_test, y_test, num_sample_test, num_diff_test)
 
-fm.save_pickle((x,y), options.outfile)
+    hist_training_outfile = options.outfilestem + '_hist_training.pkl'
+    hist_test_outfile = options.outfilestem + '_hist_test.pkl'
+    diff_training_outfile = options.outfilestem + '_diff_training.pkl'
+    diff_test_outfile = options.outfilestem + '_diff_test.pkl'
+
+    fm.save_pickle((x_train, y_train), hist_training_outfile)
+    fm.save_pickle((x_test, y_test), hist_test_outfile)
+    fm.save_pickle((x_train_diff, y_train_diff), diff_training_outfile)
+    fm.save_pickle((x_test_diff, y_test_diff), diff_test_outfile)
