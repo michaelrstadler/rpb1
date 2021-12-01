e5f23e3ebe603f3ea3b143ac1992ab8f245761c6
Michael Stadler <michaelrstadler@gmail.com>
2021-11-30 16:01:23 -0800

    Updated make_training_test_data_from_simdata.py again to us a simple difference again, but this time to orient the difference histogram so that it has a positive sum

--- bin/make_training_test_data_from_simdata.py
+++ bin/make_training_test_data_from_simdata.py
@@ -128,6 +128,12 @@ def merge_data(x_train1, x_train2, y_train1, y_train2):
     x_train_new = np.hstack((x_train1[order1], x_train2[order2]))
     return x_train_new, y_train1_new
 
+def orient_diffhist(x):
+    """Orient difference histogram so that is has a positive sum."""
+    if np.sum(x) < 0:
+        return -1 * x
+    return x
+
 def make_similarity_data(x_in, y_in, num_sample, num_diff):
     """Use random sampling to generate training data using differences
     between entries."""
@@ -147,7 +153,7 @@ def make_similarity_data(x_in, y_in, num_sample, num_diff):
         x_sub = x_in[sampled_indexes2]
         y_sub = y_in[sampled_indexes2]
         # Get differences between reference entry and sampled comparison entries.
-        x_diffs = (x_sub - x_in[index1]) ** 2
+        x_diffs = orient_diffhist(x_sub - x_in[index1])
         y_diffs = np.sum((y_sub - y_in[index1]) ** 2, axis=1) # sum of squared differences
         # Assign difference scores to empty positions in container.
         start = i * num_diff
