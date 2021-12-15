b8949cbe495c7192ec3b7cc1df4d0a40f34ed92d
Michael Stadler <michaelrstadler@gmail.com>
2021-12-14 18:26:07 -0800

    Added model saving to train_siamese_cnn.py

--- bin/train_siamese_cnn.py
+++ bin/train_siamese_cnn.py
@@ -3,7 +3,10 @@ import numpy as np
 import tensorflow as tf
 from pathlib import Path
 
-cache_dir = Path('')
+train_data_file = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/simulations/sims-test_kXXU9jhTIa'
+model_save_file = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/simulations/sims-test_model'
+
+cache_dir = Path(train_data_file)
 target_shape = (100, 100)
 channels_axis=1
 
@@ -15,3 +18,5 @@ siamese_network = fm.cnn_models.siamese_cnn.make_siamese_network(embedding)
 siamese_model = fm.cnn_models.siamese_cnn.SiameseModel(siamese_network)
 siamese_model.compile(optimizer=tf.keras.optimizers.Adam(0.0001))
 history = siamese_model.fit(train_dataset, epochs=2, validation_data=val_dataset, verbose=True)
+
+tf.saved_model.save(siamese_model, model_save_file)
