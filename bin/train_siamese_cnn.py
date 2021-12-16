4c40f1630a48366ad7c2383ea1ef1f31b7e57c14
Michael Stadler <michaelrstadler@gmail.com>
2021-12-16 10:51:33 -0800

    Added argparser to train_siamese_cnn.py

--- bin/train_siamese_cnn.py
+++ bin/train_siamese_cnn.py
@@ -4,8 +4,23 @@ import tensorflow as tf
 from pathlib import Path
 import sys
 import os
+from optparse import OptionParser
 
-train_data_folder = sys.argv[1]
+def parse_options():
+    parser = OptionParser()
+    parser.add_option("-f", "--training_data_folder", dest="training_data_folder",
+                      help="Folder containing training data.", 
+                      metavar="FOLDER")
+    parser.add_option("-e", "--num_epochs", dest="num_epochs",
+                      help="Number of epochs to train for", 
+                      metavar="NUMEPOCHS")
+    (options, args) = parser.parse_args()
+    return options
+
+options = parse_options()
+train_data_folder = options.training_data_folder
+num_epochs = int(options.num_epochs)
+
 model_save_file = os.path.join(train_data_folder, 'model')
 
 cache_dir = Path(train_data_folder)
@@ -19,7 +34,7 @@ embedding = cn.make_embedding(base_cnn)
 siamese_network = cn.make_siamese_network(embedding)
 siamese_model = cn.SiameseModel(siamese_network)
 siamese_model.compile(optimizer=tf.keras.optimizers.Adam(0.0001))
-history = siamese_model.fit(train_dataset, epochs=2, validation_data=val_dataset, verbose=True)
+history = siamese_model.fit(train_dataset, epochs=num_epochs, validation_data=val_dataset, verbose=True)
 
 tf.keras.models.save_model(embedding, model_save_file)
 #tf.saved_model.save(siamese_model, model_save_file)
