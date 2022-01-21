c41bd7aaa0fdf168adb93cd2b0d48ce2ea29bfcc
Michael Stadler <michaelrstadler@gmail.com>
2022-01-21 12:07:21 -0800

    Added multi-GPU utilization to train_siamese_cnn.py

--- bin/train_siamese_cnn.py
+++ bin/train_siamese_cnn.py
@@ -2,7 +2,7 @@
 
 
 To run on savio: 
-export PYTHONPATH="$PWD" # Put path to cnn_models here...I don't know how to manage this with savio module system yet (facepalm).
+export PYTHONPATH="$PWD/rpb1/bin/cnn_models" # Put path to cnn_models here...I don't know how to manage this with savio module system yet (facepalm).
 module unload python/3.7
 module load ml/tensorflow/2.5.0-py37
 """
@@ -36,16 +36,20 @@ model_save_file = os.path.join(train_data_folder, 'model')
 
 cache_dir = Path(train_data_folder)
 target_shape = (100, 100)
-channels_axis=1
-print('Loading training data...', end='')
-train_dataset, val_dataset = cn.make_triplet_inputs(cache_dir)
-print('finished.')
-base_cnn = cn.make_base_cnn(image_shape=(100,100))
-embedding = cn.make_embedding(base_cnn)
-siamese_network = cn.make_siamese_network(embedding)
-siamese_model = cn.SiameseModel(siamese_network)
-siamese_model.compile(optimizer=tf.keras.optimizers.Adam(0.0001))
-history = siamese_model.fit(train_dataset, epochs=num_epochs, validation_data=val_dataset, verbose=True)
 
-tf.keras.models.save_model(embedding, model_save_file)
-#tf.saved_model.save(siamese_model, model_save_file)
+#tf.debugging.set_log_device_placement(True)
+gpus = tf.config.list_logical_devices('GPU')
+strategy = tf.distribute.MirroredStrategy(gpus)
+with strategy.scope():
+    print('Loading training data...', end='')
+    train_dataset, val_dataset = cn.make_triplet_inputs(cache_dir)
+    print('finished.')
+    base_cnn = cn.make_base_cnn(image_shape=(100,100))
+    embedding = cn.make_embedding(base_cnn)
+    siamese_network = cn.make_siamese_network(embedding)
+    siamese_model = cn.SiameseModel(siamese_network)
+    siamese_model.compile(optimizer=tf.keras.optimizers.Adam(0.0001))
+    history = siamese_model.fit(train_dataset, epochs=num_epochs, validation_data=val_dataset, verbose=True)
+
+    tf.keras.models.save_model(embedding, model_save_file)
+    #tf.saved_model.save(siamese_model, model_save_file)
