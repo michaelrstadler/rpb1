81b790ff890e5062dfa37799fdee1bebde918761
Michael Stadler <michaelrstadler@gmail.com>
2022-01-27 09:20:53 -0800

    1 - combined 2d and 3d cnn functions together in siamese_cnn.py; 2 - deleted siamese_cnn_3d.py, train_siamese_cnn_3d.py, and cnn_write_dataset.py; 3 - added functions identity_block_3d, conv_block_3d, base_cnn_3d to siamese_cnn.py; 4 - rewrote make_triplet_inputs function in siamese_cnn.py to work for both mip and 3d, and to actually be correct and not try to pre-load into memory

--- bin/cnn_models/cnn_models/siamese_cnn.py
+++ bin/cnn_models/cnn_models/siamese_cnn.py
@@ -182,6 +182,175 @@ def make_base_cnn(image_shape=(100,100), name='base_cnn'):
     return Model(img_input, x, name=name)
 
 ############################################################################
+def identity_block_3d(input_tensor, kernel_size, filters, stage, block, 
+		channels_axis=-1):
+    """A block of layers that has no convolution at the shortcut.
+
+	Architecture:
+		- 1x1 convolution + batch norm + relu
+		- (k,k) convolution + batch norm + relu
+		- 1x1 convolution + batch norm
+		- Residual step: add initial input to output of previous layer
+		- relu
+    
+	Args:
+        input_tensor: input tensor
+        kernel_size: int or tuple of ints
+			The kernel size of middle conv layer at main path (default=3)
+        filters: iterable of 3 ints 
+			List of 3 integers, the number of filters for each of the 3 
+			convolution layers
+            **note: The last filter must equal be same as input (for adding 
+			in resnet shortcut)
+        stage: int
+			The current stage label, used for generating layer names
+        block: string
+			'a','b'..., current block label, used for generating layer names
+		channels_axis: int
+			Axis containing channel--used for batch normalization. Default 
+            is -1 for channels last format. For channels first, should be 1.
+
+	Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = layers.Conv3D(filters1, (1, 1, 1),
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2a')(input_tensor)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2a')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv3D(filters2, kernel_size,
+                      padding='same',
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2b')(x)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2b')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv3D(filters3, (1, 1, 1),
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2c')(x)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2c')(x)
+    x = layers.add([x, input_tensor])
+    x = layers.Activation('relu')(x)
+    return x
+
+############################################################################
+def conv_block_3d(input_tensor, kernel_size, filters, stage, block,
+    	strides=(2, 2, 2), channels_axis=-1):
+    """A block of layers that has convolution at the shortcut.
+
+	Architecture:
+		- 1x1 convolution + batch norm + relu
+		- (k,k) convolution + batch norm + relu
+		- 1x1 convolution + batch norm
+		- Residual step: 
+			-(k,k) convolution of initial input + batch norm
+			- add conv of input to output of previous layer
+		- relu
+    
+	Args:
+        input_tensor: input tensor
+        kernel_size: int or tuple of ints
+			The kernel size of middle conv layer at main path (default=3)
+        filters: iterable of 3 ints 
+			List of 3 integers, the number of filters for each of the 3 
+			convolution layers
+            **note: The last filter must equal be same as input (for adding 
+			in resnet shortcut)
+        stage: int
+			The current stage label, used for generating layer names
+        block: string
+			'a','b'..., current block label, used for generating layer names
+		strides: int or iterable of ints
+			Strides for the two non-1x1 conv layers in the block (middle and
+			shortcut)
+		channels_axis: int
+			Axis containing channel. Default is -1 for channels last format.
+            For channels first, should be 1.
+
+	Returns
+        Output tensor for the block. 
+    """
+    filters1, filters2, filters3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = layers.Conv3D(filters1, (1, 1, 1), strides=strides,
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2a')(input_tensor)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2a')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv3D(filters2, kernel_size, padding='same',
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2b')(x)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2b')(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv3D(filters3, (1, 1, 1),
+                      kernel_initializer='he_normal',
+                      name=conv_name_base + '2c')(x)
+    x = layers.BatchNormalization(axis=channels_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = layers.Conv3D(filters3, (1, 1, 1), strides=strides,
+                             kernel_initializer='he_normal',
+                             name=conv_name_base + '1')(input_tensor)
+    shortcut = layers.BatchNormalization(
+        axis=channels_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = layers.Activation('relu')(x)
+    return x
+
+############################################################################
+def make_base_cnn_3d(image_shape=(20, 100,100), name='base_cnn'):
+    """Make a CNN network for a single image.
+
+    Args:
+        image_shape: tuple of ints
+            Shape of input images in pixels
+        name: string
+            Name for model
+
+    Returns:
+        Keras model
+    """
+    img_input = layers.Input(shape=image_shape + (1,)) # Channels last.
+    x = layers.ZeroPadding3D(padding=(1, 3, 3), name='conv1_pad')(img_input)
+    
+    x = layers.Conv3D(64, (3,7,7),
+                        strides=(2, 2, 2),
+                        padding='valid',
+                        kernel_initializer='he_normal',
+                        name='conv1')(x)
+    
+    x = layers.BatchNormalization( name='bn_conv1')(x)
+    x = layers.Activation('relu')(x)
+    x = layers.ZeroPadding3D(padding=(1, 1, 1), name='pool1_pad')(x)
+    x = layers.MaxPooling3D((3, 3, 3), strides=(2, 2, 2))(x)
+
+    
+    x = conv_block_3d(x, (3,3,3), [64, 64, 256], stage=2, block='a', strides=(2,1,1))
+    x = identity_block_3d(x, (3,3,3), [64, 64, 256], stage=2, block='b')
+    
+    x = identity_block_3d(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block_3d(x, (3,3,3), [128, 128, 512], stage=3, block='a', strides=(2,2,2))
+    x = identity_block_3d(x, (3,3,3), [128, 128, 512], stage=3, block='b')
+    x = identity_block_3d(x, (3,3,3), [128, 128, 512], stage=3, block='c')
+    x = identity_block_3d(x, (3,3,3), [128, 128, 512], stage=3, block='d')
+
+    x = conv_block_3d(x, (3,3,3), [512, 512, 2048], stage=4, block='a', strides=(2,2,2))
+    x = identity_block_3d(x, (3,3,3), [512, 512, 2048], stage=4, block='b')
+    x = identity_block_3d(x, (3,3,3), [512, 512, 2048], stage=4, block='c')
+    
+    return Model(img_input, x, name=name)
+
+############################################################################
 def make_embedding(input_model, name='base_cnn'):
     """Make a CNN (from base_cnn) that creates an embedding for a 
       single image.
@@ -322,37 +491,78 @@ class SiameseModel(Model):
         return [self.loss_tracker]
 
 ############################################################################
-def make_triplet_inputs(folder, repeats=1):
+def make_triplet_inputs(folder, n_repeats=1, mip=True):
 
-    def preprocess_image(filename):
+    def preprocess_image(filename, mip=True):
         """
         Load the specified file as a JPEG image, preprocess it and
         resize it to the target shape.
         """
         a = str(filename)
-        _,filename,_ = a.split("'")
+        _, filename, _ = a.split("'")
         with open(filename, 'rb') as file:
             im = pickle.load(file)
-
-        im = im.max(axis=0)
+        if mip:
+            im = im.max(axis=0)
         im = im.astype('float32')
         im = np.expand_dims(im, axis=-1)
         # Normalize 0-1.
         im = (im - np.min(im)) / (np.max(im) - np.min(im))
         return im
 
-    def preprocess_triplets(anchor, positive, negative):
+    def preprocess_triplets_mip(anchor, positive, negative):
         """
         Given the filenames corresponding to the three images, load and
         preprocess them.
         """
-        return (
-            preprocess_image(anchor),
-            preprocess_image(positive),
-            preprocess_image(negative)
+        [anchor,] = tf.py_function(preprocess_image,[anchor,],[tf.float32,])
+        [positive,] = tf.py_function(preprocess_image,[positive,],[tf.float32,])
+        [negative,] = tf.py_function(preprocess_image,[negative,],[tf.float32,])
+        return (anchor, positive, negative)
+    
+    def preprocess_triplets_3d(anchor, positive, negative):
+        """
+        Given the filenames corresponding to the three images, load and
+        preprocess them.
+        """
+        preprocess_image_3d = lambda x: preprocess_image(x, mip=False)
+        [anchor,] = tf.py_function(preprocess_image_3d,[anchor,],[tf.float32,])
+        [positive,] = tf.py_function(preprocess_image_3d,[positive,],[tf.float32,])
+        [negative,] = tf.py_function(preprocess_image_3d,[negative,],[tf.float32,])
+        return (anchor, positive, negative)
+
+    def tf_random_rotate_image(image, mip=True):
+        """Apply random rotation -30 to 30 degrees to image."""
+        def random_rotate_image_mip(image):
+            return ndimage.rotate(image, np.random.RandomState().uniform(-30, 30), reshape=False)
+        def random_rotate_image_3d(image):
+            return ndimage.rotate(image, np.random.RandomState().uniform(-30, 30), axes=(1,2), reshape=False)
+
+        im_shape = image.shape
+        if mip:
+            [image,] = tf.py_function(random_rotate_image_mip, [image], [tf.float32])
+        if not mip:
+            [image,] = tf.py_function(random_rotate_image_3d, [image], [tf.float32])
+        image.set_shape(im_shape)
+        return image
+    
+    def random_rotate_triplets_mip(anchor, positive, negative):
+        """Mappable function to apply random rotate to mip dataset."""
+        return(
+            tf_random_rotate_image(anchor),
+            tf_random_rotate_image(positive),
+            tf_random_rotate_image(negative)
+        )
+    
+    def random_rotate_triplets_3d(anchor, positive, negative):
+        """Mappable function to apply random rotate to 3d dataset."""
+        return(
+            tf_random_rotate_image(anchor, mip=False),
+            tf_random_rotate_image(positive, mip=False),
+            tf_random_rotate_image(negative, mip=False)
         )
 
-    #cache_dir = Path('/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/simulations/keras_test')
+    # Set up input directories.
     cache_dir=folder
     anchor_images_path = cache_dir / "left"
     positive_images_path = cache_dir / "right"
@@ -369,32 +579,43 @@ def make_triplet_inputs(folder, repeats=1):
 
     image_count = len(anchor_images)
 
+    # Create datasets from these sorted files. These are in order and match in pairs.
     anchor_dataset = tf.data.Dataset.from_tensor_slices(anchor_images)
     positive_dataset = tf.data.Dataset.from_tensor_slices(positive_images)
 
-    # To generate the list of negative images, let's randomize the list of
+    # To generate the list of negative images, randomize the list of
     # available images and concatenate them together.
     rng = np.random.RandomState(seed=42)
     rng.shuffle(anchor_images)
     rng.shuffle(positive_images)
-
     negative_images = anchor_images + positive_images
     np.random.RandomState(seed=32).shuffle(negative_images)
 
+    # Make negative dataset, apply shuffle so these will be shuffled 
+    # when iterated over.
     negative_dataset = tf.data.Dataset.from_tensor_slices(negative_images)
-    negative_dataset = negative_dataset.shuffle(buffer_size=4096)
+    negative_dataset = negative_dataset.shuffle(buffer_size=4096)    
 
+    # Combine datasets to form triplet images, apply shuffle to the whole
+    # dataset. So upon iteration, order of triplets will be random, and
+    # within triplets, the negative image will shuffle.
     dataset = tf.data.Dataset.zip((anchor_dataset, positive_dataset, negative_dataset))
     dataset = dataset.shuffle(buffer_size=1024)
-    def preprocess_triplets_py_function(a, p, n):
-        [anchor, positive, negative] = tf.py_function(preprocess_triplets, [a,p,n], [tf.float32,tf.float32,tf.float32])
-        return (anchor, positive, negative)
 
-    dataset = dataset.map(preprocess_triplets_py_function)
+    # Apply preprocessing and rotation via special mappable functions.
+    if mip:
+        dataset = dataset.map(preprocess_triplets_mip)
+        dataset = dataset.map(random_rotate_triplets_mip)
 
-    # Let's now split our dataset in train and validation.
-    train_dataset = dataset.take(round(image_count * 0.8))
-    val_dataset = dataset.skip(round(image_count * 0.8))
+    if not mip:
+        dataset = dataset.map(preprocess_triplets_3d)
+        dataset = dataset.map(random_rotate_triplets_3d)
+    
+    dataset = dataset.repeat(n_repeats)
+    # Divide into training and evaluation, batch and prefetch.
+    train_dataset = dataset.take(round(image_count * n_repeats * 0.8))
+    val_dataset = dataset.skip(round(image_count * n_repeats * 0.8))
+    #train_dataset = train_dataset.repeat(n_repeats)
 
     train_dataset = train_dataset.batch(32, drop_remainder=False)
     train_dataset = train_dataset.prefetch(8)
--- bin/train_siamese_cnn.py
+++ bin/train_siamese_cnn.py
@@ -27,35 +27,47 @@ def parse_options():
     parser.add_option("-r", "--num_repeats", dest="num_repeats", default=1,
                       help="Number of repeats of the dataset to use. Repeats will have shuffled negative images and be rotated.", 
                       metavar="NUMREPEATS")
+    parser.add_option("-m", action="store_true", dest="mip",
+                      help="Flag: write maximum intensity projection (mip).")
     (options, args) = parser.parse_args()
     return options
+
+def get_target_shape(dataset):
+    iter = dataset.as_numpy_iterator()
+    i = next(iter)
+    shape = i[0].shape[1:-1]
+    return shape
 
 options = parse_options()
 train_data_folder = options.training_data_folder
 num_epochs = int(options.num_epochs)
 num_repeats = int(options.num_repeats)
+mip = options.mip
 
 model_save_file = os.path.join(train_data_folder, 'model')
 
 cache_dir = Path(train_data_folder)
-target_shape = (100, 100)
 
 sys.stdout.write('Loading training data...\n')
 sys.stdout.flush()
 t1 = time()
-train_dataset, val_dataset = cn.make_triplet_inputs(cache_dir, num_repeats)
+
+train_dataset, val_dataset = cn.make_triplet_inputs(cache_dir, num_repeats, mip=mip)
+
 t2 = time()
 sys.stdout.write('finished.\n')
 sys.stdout.write('time: ' + str(t2 - t1) + '\n')
 sys.stdout.flush()
-train_dataset_folder = os.path.join(train_data_folder, 'training_dataset')
-val_dataset_folder = os.path.join(train_data_folder, 'val_dataset')
-if not os.path.isdir(train_dataset_folder):
-    os.mkdir(train_dataset_folder)
-if not os.path.isdir(val_dataset_folder):
-    os.mkdir(val_dataset_folder)
 
-base_cnn = cn.make_base_cnn(image_shape=target_shape)
+target_shape = get_target_shape(val_dataset)
+print(target_shape)
+
+if mip:
+    base_cnn = cn.make_base_cnn(image_shape=target_shape)
+
+if not mip:
+    base_cnn = cn.make_base_cnn_3d(image_shape=target_shape)
+
 embedding = cn.make_embedding(base_cnn)
 siamese_network = cn.make_siamese_network(embedding)
 siamese_model = cn.SiameseModel(siamese_network)
