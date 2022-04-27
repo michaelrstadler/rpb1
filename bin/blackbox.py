a72cb6c14748e19a7811e001244666ccb5d61dd1
Michael Stadler <michaelrstadler@gmail.com>
2022-04-27 16:36:46 -0700

    Trying model3

--- bin/blackbox.py
+++ bin/blackbox.py
@@ -16,7 +16,7 @@ t1 = time()
 # Add files and folders.
 reals_folder = '/global/home/users/mstadler/scratch/blackbox/reals/'
 sims_folder = '/global/home/users/mstadler/scratch/blackbox/sims/'
-model_file = '/global/home/users/mstadler/scratch/blackbox/model2'
+model_file = '/global/home/users/mstadler/scratch/blackbox/model3'
 #reals_folder = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/blackbox/reals/'
 #sims_folder = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/blackbox/sims/'
 #model_file = '/Users/michaelstadler/Bioinformatics/Projects/rpb1/data/blackbox/model'
@@ -28,6 +28,7 @@ def preprocess_images(im_sim, im_real):
     [sim,] = tf.py_function(cn.preprocess_image,[im_sim,],[tf.float32,])
     [real,] = tf.py_function(cn.preprocess_image,[im_real,],[tf.float32,])
     return sim, real
+
 
 # Make file lists from input folders.
 sim_files1 = os.listdir(sims_folder)
@@ -51,13 +52,19 @@ sim_ds = sim_ds.batch(batch_size, drop_remainder=False)
 num_elem = np.product(image_shape)
 img_input = layers.Input(shape=image_shape + (1,)) # Channels last.
 #x = layers.ZeroPadding3D(padding=(5, 10, 10), name='psf_pad')(img_input)
-x = layers.Conv3D(1, (8,15,15),
+x = layers.Conv3D(1, (8,24,24),
         strides=(1, 1, 1),
         padding='same',
         kernel_initializer='he_normal',
         name='psf')(img_input)
 
-x = layers.Conv3D(64, (3,3,3),
+x = layers.Conv3D(256, (3,3,3),
+        strides=(1, 1, 1),
+        padding='same',
+        kernel_initializer='he_normal',
+        name='conv1')(x)
+
+x = layers.Conv3D(1, (1,1,1),
         strides=(1, 1, 1),
         padding='same',
         kernel_initializer='he_normal',
